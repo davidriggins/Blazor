1965d78590395341209890e3e9a3a49e9ae82b44
David Riggins <driggins@esri.com>
2022-06-06 11:09:06 -0700

    Section 3: Lesson 27 - Dependency Injection.

--- BlazorMovies/BlazorMovies/Client/Program.cs
+++ BlazorMovies/BlazorMovies/Client/Program.cs
@@ -8,4 +8,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+ConfigureServices(builder.Services);
+
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton<SingletonService>();
+    services.AddTransient<TransientService>();
+}
