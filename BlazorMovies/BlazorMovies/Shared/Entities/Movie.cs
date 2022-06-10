b3519bfae86496957be49f317532886708fff2e6
David Riggins <driggins@esri.com>
2022-06-10 11:46:37 -0700

    Section 5: Lesson 59 - Building a Movie Form.

--- BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
+++ BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace BlazorMovies.Shared.Entities
 {
     public class Movie
     {
-        public int Id { get; set; } = 1;
+        public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        public string Summary { get; set; }
+        public bool InTheaters { get; set; }
+        public string Trailer { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
         public string Poster { get; set; }
         public string TitleBrief 
         { 
