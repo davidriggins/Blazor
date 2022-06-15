2d9b22b5cbf259d0b0946cd8630e027046613677
David Riggins <driggins@esri.com>
2022-06-15 12:34:46 -0700

    Section 6: Lesson 66 - Creating the Database.

---
+++ BlazorMovies/BlazorMovies/Server/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BlazorMovies.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorMovies.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Use composite key for these tables
+            modelBuilder.Entity<MoviesActors>().HasKey(x => new { x.MovieId, x.PersonId });
+            modelBuilder.Entity<MoviesGenres>().HasKey(x => new { x.MovieId, x.GenresId });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Person> Actors { get; set; }
+        public DbSet<MoviesActors> MoviesActors { get; set; }
+        public DbSet<MoviesGenres> MoviesGenres { get; set; }
+    }
+}
